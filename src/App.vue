d3bd45138c67bba448d3d0a40085cdf8cf111059
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-05-26 12:34:21 +0200

    publish

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    // The configuration of the editor.
+                }
+            };
+        }
 }
 </script>
 
